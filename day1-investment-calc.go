0ac6a13c9f3be85fedf23bfd61ebd00faa332790
aman jaiswal <iaman.aj7@gmail.com>
2024-07-30 21:13:19 +0530

    added statements to take user inputs

--- day1-investment-calc.go
+++ day1-investment-calc.go
@@ -7,10 +7,19 @@ import(
 
 func main(){
   
-  const inflationRate = 4.8
-  var   investmentAmount float64 = 10000000
-  years := 10.0
-  var expectedReturnRate float64 = 16
+  var   investmentAmount, years, expectedReturnRate, inflationRate float64
+
+  fmt.Println("InflationRate is: ")
+  fmt.Scan(&inflationRate)
+
+  fmt.Println("Time duration in years: ")
+  fmt.Scan(&years)
+
+  fmt.Println("expected return rate is: ")
+  fmt.Scan(&expectedReturnRate)
+
+  fmt.Println("investment amount is: ")
+  fmt.Scan(&investmentAmount)
 
   futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
